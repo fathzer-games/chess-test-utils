21e2ff15a9a0cbf741419c6f8600d815f538ca97
Astesana <jm-dev@astesana.net>
2025-03-26 10:36:28 +0100

    Remove unused import

--- src/test/java/com/fathzer/chess/utils/test/ExcludeMethodsConditionTest.java
+++ src/test/java/com/fathzer/chess/utils/test/ExcludeMethodsConditionTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ConditionEvaluationResult;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-import com.fathzer.chess.utils.test.ExcludeMethods;
-
 class ExcludeMethodsConditionTest {
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
